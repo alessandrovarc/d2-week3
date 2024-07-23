3f5c3e5199cb401f88e828bc05eb513d3761b1e5
alessandrovarc-git <alessandro.varchetta1996@gmail.com-git>
2024-07-23 16:44:26 +0200

    più timer

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const inputValue = inputField.value;
 const nameP = document.getElementById('nameP')
 nameP.innerText = localStorage.getItem("name");
 
+
+const contator = document.getElementById('contator');
+let contatorValue = sessionStorage.getItem('contator') || 0;
+contator.innerText=contatorValue;
+
 function saveClick() {
     const inputValue = inputField.value;
     localStorage.setItem("name", inputValue)
@@ -14,5 +19,13 @@ function deleteClick () {
     localStorage.removeItem("name")
 }
 
-
+function contatorTimer () {
+    setInterval (()=> {
+        contatorValue ++;
+        contator.innerText = contatorValue;
+        sessionStorage.setItem("contator", contatorValue);
+    },1000);
+}
+ 
+window.onload = contatorTimer();
 
